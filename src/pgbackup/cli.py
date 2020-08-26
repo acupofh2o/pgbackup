c09141a79b9162c9bf3490a3925d530507446124
Ubuntu <unisilvium@gmail.com>
2020-08-26 08:48:57 +0000

    added filenames to backups, added timestamps to backups, added -d driver sellection shorthand

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -15,5 +15,22 @@ def create_parser():
     Backup PostgreSQL databases locally or to AWS S3
     """)
     parser.add_argument("url", help="URL of database to backup")
-    parser.add_argument("--driver", help="how & where to store backup", nargs=2, action=DriverAction, required= True)
+    parser.add_argument("--driver", "-d", help="how & where to store backup", nargs=2, metavar=("DRIVER","DESTINATION"), action=DriverAction, required= True)
     return parser
+
+def main():
+    import boto3
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)i
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M", local.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing up db to {args.destination} in S3 with filename: {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        print(f"Backing up db locally with the name: {outfile.namme}")
+        storage.local(dump.stdout, outfile)
